55f65b815b0a66333852a08e1f06194b9ac20d5a
Max Wang <max.wang.cs@gmail.com>
2017-01-15 11:34:36 -0800

    Initial Commit

---
+++ NYTimes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NYTimes
+//
+//  Created by Sida Wang on 1/15/17.
+//  Copyright © 2017 Sida Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NYTimes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NYTimes
+//
+//  Created by Sida Wang on 1/15/17.
+//  Copyright © 2017 Sida Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
